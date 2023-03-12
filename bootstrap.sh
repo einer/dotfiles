e952e78a8fb87c6b865d20d47bfba00a6f476704
Otmar Hilliges <otmar.hilliges@inf.ethz.ch>
2023-03-12 15:56:40 +0100

    installing stuff on apply; script termination not super clean, because installing oh-my-zsh launches new zsh shell

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,11 +12,11 @@ if [ ! -f "$HOME/.config/chezmoi/chezmoi.toml" ]; then
   chmod 0600 "$HOME/.config/chezmoi/chezmoi.toml"
 fi
 
-if [ -f "$HOME/Brewfile" ]; then
-  echo "Installing tools and apps from Brewfile ..."
-  if brew bundle --file="$HOME/Brewfile"; then
-    echo "All items in Brewfile were installed successfully."
-  else
-    echo "Some items in Brewfile were not installed successfully."
-  fi
-fi
+#if [ -f "$HOME/Brewfile" ]; then
+#  echo "Installing tools and apps from Brewfile ..."
+#  if brew bundle --file="$HOME/Brewfile"; then
+#    echo "All items in Brewfile were installed successfully."
+#  else
+#    echo "Some items in Brewfile were not installed successfully."
+#  fi
+#fi
