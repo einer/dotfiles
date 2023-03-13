0d2e37a2a5f8233074c9b31c98088302e6fb0637
Otmar Hilliges <otmar.hilliges@inf.ethz.ch>
2023-03-13 12:59:00 +0100

    adding scripts to install pre-requisites; password-manager and to maintain base brew bundles

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,15 +8,7 @@ brew bundle --file=- <<EOF
 EOF
 if [ ! -f "$HOME/.config/chezmoi/chezmoi.toml" ]; then
   echo "Applying dotfiles with chezmoi ..."
-  chezmoi init --apply --verbose https://github.com/einer/dotfiles.git
+  chezmoi init --apply --verbose git@github.com/einer/dotfiles.git
   chmod 0600 "$HOME/.config/chezmoi/chezmoi.toml"
 fi
 
-#if [ -f "$HOME/Brewfile" ]; then
-#  echo "Installing tools and apps from Brewfile ..."
-#  if brew bundle --file="$HOME/Brewfile"; then
-#    echo "All items in Brewfile were installed successfully."
-#  else
-#    echo "Some items in Brewfile were not installed successfully."
-#  fi
-#fi
